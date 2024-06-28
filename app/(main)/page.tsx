54ad088bec9cda92c6187d2697272b7aed1f5452
mayur shinde <kssmsslss330@gmail.com>
2024-06-28 18:14:39 +0530

    "fixed tit"

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { CurrentUser } from "@/hooks/use-current-user";
  
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+const currentUser = CurrentUser();
+ if(!currentUser){
+  redirect("/auth/login");
+ }
 
-redirect("/posts")
+ if(currentUser){
+  redirect("/posts")
+ }
   return (
-  <Button>mayur</Button>
- 
+  <Button> </Button> 
   );
 }
