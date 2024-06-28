52c8fa76f96a447c1dc638d26343938ac68f1678
mayur shinde <kssmsslss330@gmail.com>
2024-06-28 20:20:43 +0530

    "final commit"

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,36 @@
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+import { AuthRoutes, DEFAULT_LOGIN_REDIRECT, apiAuthprefix, publicRoutes } from "./route";
 
- 
-import { NextResponse } from "next/server"
-import next from "next"
-import { AuthRoutes, DEFAULT_LOGIN_REDIRECT, apiAuthprefix, publicRoutes } from "./route"
-
-export const { auth } = NextAuth(authConfig)
+export const { auth } = NextAuth(authConfig);
 
 export default auth((req: any) => {
-  const { nextUrl, user } = req;
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
- 
+
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthprefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = AuthRoutes.includes(nextUrl.pathname); 
- 
- 
+
   if (isApiAuthRoute) {
-    return null;  
+    return NextResponse.next();
   }
 
-  if (isAuthRoute ) {
-      
- if(isLoggedIn ){ 
-    return Response.redirect(new URL( "/posts", nextUrl)); 
- }
- return null;
-    } 
-    
-    
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/posts", nextUrl));
+    }
+    return NextResponse.next();
+  }
+
   if (!isLoggedIn && !isPublicRoute) {
-      return Response.redirect(new URL("/auth/login", nextUrl));
- }
-  
-    return null;  
-  
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return NextResponse.next();
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
